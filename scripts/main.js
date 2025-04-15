85c952c48e84d6aa5f2e72b467eea049f2a3b087
denyschr <denprogman99@gmail.com>
2025-04-15 16:54:42 +0300

    style: add editorconfig, run format

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,19 @@
 const body = document.body;
-const overlay = document.querySelector('[data-overlay]');
-const navbarMenu = document.querySelector('[data-navbar-menu]');
-const navbarToggleButton = document.querySelector('[data-navbar-toggle-button]');
-
+const overlay = document.querySelector("[data-overlay]");
+const navbarMenu = document.querySelector("[data-navbar-menu]");
+const navbarToggleButton = document.querySelector(
+  "[data-navbar-toggle-button]"
+);
 
 function toggleMenu() {
-	body.classList.toggle('lock');
-	overlay.classList.toggle('active');
-	navbarMenu.classList.contains('open')
-		? navbarToggleButton.setAttribute('aria-expanded', 'false')
-		: navbarToggleButton.setAttribute('aria-expanded', 'true');
-	navbarMenu.classList.toggle('open');
-	navbarToggleButton.classList.toggle('active');
+  body.classList.toggle("lock");
+  overlay.classList.toggle("active");
+  navbarMenu.classList.contains("open")
+    ? navbarToggleButton.setAttribute("aria-expanded", "false")
+    : navbarToggleButton.setAttribute("aria-expanded", "true");
+  navbarMenu.classList.toggle("open");
+  navbarToggleButton.classList.toggle("active");
 }
 
-navbarToggleButton.addEventListener('click', toggleMenu);
-overlay.addEventListener('click', toggleMenu);
+navbarToggleButton.addEventListener("click", toggleMenu);
+overlay.addEventListener("click", toggleMenu);
